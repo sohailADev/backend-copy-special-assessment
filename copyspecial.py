51fa8b38702ff8766cc18b5ebde12fb116584655
sohail aslam <sohailaslam0707@gmail.com>
2020-06-18 20:12:50 -0400

    ready for grade

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "sohailadev"
 
 import re
 import os
@@ -19,17 +19,33 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    files = []
+    for file in os.listdir(dirname):
+        special_file = re.findall(r'__(\w+)__', file)
+        if special_file:
+            files.append(os.path.abspath(os.path.join(dirname, file)))
+
+    return files
 
 
 def copy_to(path_list, dest_dir):
+    """Given a dirname, returns a list of all its special files."""
     # your code here
+    if not os.path.isdir(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        shutil.copy(path, dest_dir)
     return
 
 
 def zip_to(path_list, dest_zip):
+    """Given a dirname, returns a list of all its special files."""
     # your code here
+    print("ZIP - TOOOOOOOO")
+    for path in path_list:
+        print(f'zip -j {dest_zip} {path}')
+        subprocess.run(['zip', '-j', dest_zip, path])
+
     return
 
 
@@ -40,6 +56,7 @@ def main(args):
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='find dir for speical files')
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
@@ -51,6 +68,15 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
+    if not ns:
+        parser.print_usage()
+    path_list = get_special_paths(ns.from_dir)
+    if ns.todir:
+        copy_to(path_list, ns.todir)
+    elif ns.tozip:
+        zip_to(path_list, ns.tozip)
+    else:
+        print(*path_list, sep='\n')
 
 
 if __name__ == "__main__":
